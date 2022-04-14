14fe19041150959cb5543bf9ac89bb510575d51a
ihossen016 <ihossen016@gmail.com>
2022-04-14 11:32:25 +0600

    searchbar component created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import "./Navbar.css";
 
 import React from "react";
 import { Link } from "react-router-dom";
+import Searchbar from "./Searchbar";
 
 export default function Navbar() {
   return (
@@ -11,6 +12,7 @@ export default function Navbar() {
         <Link className="brand" to="/">
           <h1>Recipe Directory</h1>
         </Link>
+        <Searchbar />
         <Link to="/create">Create Recipe</Link>
       </nav>
     </div>
