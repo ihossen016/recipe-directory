588951fb54ca76949511dbe48c8b093954a7ba3f
ihossen016 <ihossen016@gmail.com>
2022-04-14 11:54:39 +0600

    search page complete

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,6 +1,26 @@
+import { useLocation } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+import RecipeList from "../../components/RecipeList";
+
 // Styles
 import "./Search.css";
 
 export default function Search() {
-  return <div>Search</div>;
+  // Extracting query parameter from url string
+  const queryString = useLocation().search;
+  const queryPrams = new URLSearchParams(queryString);
+  const query = queryPrams.get("q");
+
+  // Fetching Data according to query match
+  const url = "http://localhost:3000/recipes?q=" + query;
+  const { data, isPending, error } = useFetch(url);
+
+  return (
+    <div>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
+    </div>
+  );
 }
