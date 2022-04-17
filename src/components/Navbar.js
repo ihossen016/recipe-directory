327a5acc7e1960e3813acd721b94ed0f4f6d13e6
ihossen016 <ihossen016@gmail.com>
2022-04-17 11:37:56 +0600

    reducer created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ import { useTheme } from "../hooks/useTheme";
 import "./Navbar.css";
 
 export default function Navbar() {
-  const { color } = useTheme();
+  const { color, changeColor } = useTheme();
 
   return (
     <div className="navbar" style={{ background: color }}>
-      <nav>
+      <nav onClick={() => changeColor("pink")}>
         <Link className="brand" to="/">
           <h1>Recipe Directory</h1>
         </Link>
