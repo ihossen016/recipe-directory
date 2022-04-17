deb2746aa5d8ef486e717d2e02fb07bdc2f15688
ihossen016 <ihossen016@gmail.com>
2022-04-17 12:32:31 +0600

    mode change created & project completed

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import { useTheme } from "../hooks/useTheme";
 
 // Styles
 import "./RecipeList.css";
 
 export default function RecipeList({ recipes }) {
+  const { mode } = useTheme();
+
   // Checking if any recipe is there
   if (recipes.length === 0) {
     return <div className="error">No recipes to load...</div>;
@@ -12,7 +15,7 @@ export default function RecipeList({ recipes }) {
   return (
     <div className="recipe-list">
       {recipes.map(recipe => (
-        <div key={recipe.id} className="card">
+        <div key={recipe.id} className={`card ${mode}`}>
           <h3>{recipe.title}</h3>
           <p>{recipe.cookingTime} to cook this.</p>
           <div>{recipe.method.substring(0, 100)}...</div>
